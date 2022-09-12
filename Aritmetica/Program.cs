92d36c61fee637fa2ca0b336a0007a5469f82a43
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-12 12:04:36 -0300

    Artimetica.cs
    Valor absoluto
    Raiz cuadrada y demas

---
+++ Aritmetica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Aritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double absoluto, potencia, raiz, numero;
+            Console.Write("Ingrese un numero real: ");
+            int real = Convert.ToInt32(Console.ReadLine());
+
+            numero = real / 2;
+
+            absoluto = Math.Abs(real);
+
+            potencia = Math.Pow(real, 10);
+
+            raiz = Math.Sqrt(real);
+
+            Console.WriteLine("El resto: " + numero);
+            Console.WriteLine("Valor Absoluto: " + absoluto);
+            Console.WriteLine("Decima Potencia: " + potencia);
+            Console.WriteLine("Raiz Cuadrada: " + raiz);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
